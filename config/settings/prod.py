a9aa52e69eb03f7070a0addcc8f8ecdfab7e8409
Kenny Lepping <kennylepping@gmail.com>
2022-02-26 00:03:23 -0500

    Testing different static and media files settings

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -37,15 +37,17 @@ AWS_HEADERS = {
     'Access-Control-Allow-Origin': '*',
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-
-PUBLIC_MEDIA_LOCATION = 'media'
 
 
 # Heroku Logging
